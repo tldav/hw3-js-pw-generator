61a93f4ea9e387b38a99b091e61624b7adef2663
Neverender <tldavis09@yahoo.com>
2020-01-12 21:12:25 -0600

    .

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
-var displayPassword = document.querySelector("#display-pw");
+var passwordEl = document.querySelector(".display-pw");
+var generateBtn = document.querySelector(".btn1");
+var copyBtn = document.querySelector(".btn2");
 
 // Possible characters
 var charLow = "abcdefghijklmnopqrstuvwxyz";
@@ -12,6 +14,51 @@ var randomFunction = {
 	num: getNum,
 	sym: getSym
 };
+// Generate Password button starts
+generateBtn.addEventListener("click", function() {
+	alert(
+		"Please select character types and number of characters to generate a random password."
+	);
+	var selLower = confirm("Include lowercase?");
+	var selUpper = confirm("Include uppercase?");
+	var selNum = confirm("Include numbers?");
+	var selSym = confirm("Include Symbols");
+	var selLength = +prompt("Enter a number between 8 and 128");
+
+	passwordEl.innerHTML = generatePassword(
+		selLower,
+		selUpper,
+		selNum,
+		selSym,
+		selLength
+	);
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+	// Random password is stored here
+	var randomPassword = "";
+	// Counts the number of character types
+	var typesCount = lower + upper + number + symbol;
+	// Array of objects filters out character types not chosen (false)
+	var typesArray = [{ lower }, { upper }, { number }, { symbol }].filter(
+		item => Object.values(item)[0]
+	);
+	if (typesCount === 0 || length < 8 || length > 128) {
+		alert("You must select at least one character type.");
+		return "Your Secure Password";
+	}
+
+	for (var i = 0; i < length; i += typesCount) {
+		typesArray.forEach(type => {
+			var functionName = Object.keys(type)[0];
+			console.log(functionName);
+
+			randomPassword += randomFunction[functionName]();
+		});
+	}
+
+	console.log(generatePassword());
+}
 
 // Generates random lowercase letter
 function getLower() {
@@ -29,14 +76,3 @@ function getNum() {
 function getSym() {
 	return sym[Math.floor(Math.random() * sym.length)];
 }
-
-console.log(getLower(), getUpper(), getNum(), getSym());
-
-// Add
-// document.getElementById("display-pw").value = password;
-
-function getThing() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-// console.log(getThing());
